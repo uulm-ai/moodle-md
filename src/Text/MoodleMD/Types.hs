2b481b0103e01d9d3523c026994a17c1dbf46cd0
Thomas Geier <thomas.geier@uni-ulm.de>
2014-11-28 09:40:15 +0100

    disable truefalse question type

--- src/Text/MoodleMD/Types.hs
+++ src/Text/MoodleMD/Types.hs
@@ -11,25 +11,27 @@ data AnswerProp = AnswerProp Int [Block] deriving Show
 -- |A set of answers that can be used to build a question.
 data Answers 
     = Multichoice [(Text,AnswerProp)]
-    | TrueFalse   [(Text,AnswerProp)]
-    | Numerical   [((NumType,NumType),AnswerProp)] -- ^Numerical answer with (value,tolerance) tuples as answer options.
+-- not correctly implemented
+--    | TrueFalse   [(Text,AnswerProp)]
+    -- |Numerical answer with (value,tolerance) tuples as answer options.
+    | Numerical   [((NumType,NumType),AnswerProp)]
     deriving Show
 
 -- |Return the answer type as a String.
 answerType :: Answers -> String
 answerType (Multichoice _) = "multichoice"
-answerType (TrueFalse _) = "truefalse"
+-- answerType (TrueFalse _) = "truefalse"
 answerType (Numerical _) = "numerical"
 
 -- |Return the list of answers, independent of question type.
 answerContent :: Answers -> Either [(Text,AnswerProp)] [((NumType,NumType),AnswerProp)]
 answerContent (Multichoice x) = Left x
-answerContent (TrueFalse x) = Left x
+-- answerContent (TrueFalse x) = Left x
 answerContent (Numerical x) = Right x
 
 makeStringAnswers :: String -> [(Text,AnswerProp)] -> Either String Answers
 makeStringAnswers "multichoice" as = Right $ Multichoice as
-makeStringAnswers "truefalse"   as = Right $ TrueFalse as
+-- makeStringAnswers "truefalse"   as = Right $ TrueFalse as
 makeStringAnswers t _ = Left $ "unknown question type: " ++ t
 
 -- |A question body consisting of a title, a Body, and an Answer option.
