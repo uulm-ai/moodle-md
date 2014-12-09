20717316221f4b84ce8cf1c409fe49bd31251823
Thomas Geier <thomas.geier@uni-ulm.de>
2014-12-09 16:59:00 +0100

    add simple CLI interface (fixes moodle-md.cabal)

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,22 @@ import Text.MoodleMD.Types
 import Text.MoodleMD.Reader
 import Text.MoodleMD.WriterXML
 import System.Environment
-import Control.Applicative ((<$>))
+import Control.Applicative
+import System.Console.ArgParser
+
+data Config = Config {input :: FilePath} deriving Show
+
+cliParser :: ParserSpec Config
+cliParser = Config
+    `parsedBy` optPos "" "input-file" `Descr` "input file name; leave blank for reading from stdin"
 
 main ::  IO ()
-main = do
-    f <- fmap head getArgs
-    result <- either (error.show) id <$> readMoodleMDFile  f
+main = withParseResult cliParser run
+
+run :: Config -> IO ()
+run (Config inFile) = do
+    input <- if inFile == "" then getContents else readFile inFile
+    let result = either (error.show) id . parseMoodleMD $ input
     rendered <- renderQs result
     putStrLn rendered
 
